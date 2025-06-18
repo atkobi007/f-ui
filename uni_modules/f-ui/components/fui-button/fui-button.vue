f139ed342955e3cbd871fe2ce6ede9ae517a4cb5
akobi <201678915+atkobi007@users.noreply.github.com>
2025-06-18 22:48:45 +0700

    按钮组件

--- uni_modules/f-ui/components/fui-button/fui-button.vue
+++ uni_modules/f-ui/components/fui-button/fui-button.vue
@@ -1,102 +1,156 @@
 <template name="fui-button">
-	<view class="fui-button" :class="buttonBackgroundCls" @touchstart="onTouchStart" @touchcancel="onTouchEnd" @contextmenu="onContextMenu"
+	<view class="fui-button" :style="getBackGroundColor" @touchstart="onTouchStart" @touchcancel="onTouchEnd"
 		@touchend="onTouchEnd">
-		<slot name="icon" v-if="buttonState!=1"></slot>
-		<slot name="down_icon" v-if="buttonState==1">
-			<!-- 如果未设置,按下使用默认icon -->
-			<slot name="icon"></slot>
-		</slot>
-		<text :class="buttonTextColorCls">{{text}}</text>
+		<view class="button-container">
+			<slot name="icon" v-if="iconPositon=='LEFT'"></slot>
+			<view class="button-text-container">
+				<slot name="icon" v-if="iconPositon=='TOP'"></slot>
+				<span :style="getTextColor">{{text}}</span>
+				<slot name="icon" v-if="iconPositon=='BOTTOM'"></slot>
+			</view>
+			<slot name="icon" v-if="iconPositon=='RIGHT'"></slot>
+		</view>
 	</view>
 </template>
 
 <script setup>
-	import {ref,onMounted,computed,defineProps} from "vue";
-	
-	const props=defineProps({
-		downAnimation:{
-			type:Boolean,
-			default:false
+	import {
+		ref,
+		onMounted,
+		computed,
+		defineProps
+	} from "vue";
+
+	const props = defineProps({
+		//是否按下后按钮变化
+		downAnimation: {
+			type: Boolean,
+			default: true
 		},
-		disable:{
-			type:Boolean,
-			default:false
+		//默认文字颜色
+		textColor: {
+			type: String,
+			default: "#000"
 		},
-		text:{
-			type:String,
-			default:"BUTTON"
+		//按下后文字颜色
+		downTextColor: {
+			type: String,
+			default: ""
+		},
+		//默认背景颜色,border 背景色，
+		//可设置渐变 linear-gradient(to right,red,blue)
+		//注意:设置 plain: true后不可设置为渐变色背景,否则border 不生效
+		backGroundColor: {
+			type: String,
+			default: "gray"
+		},
+		//按下后背景颜色
+		downBackGroundColor: {
+			type: String,
+			default: ""
+		},
+		//是否禁用
+		disabled: {
+			type: Boolean,
+			default: false
+		},
+		//文字内容
+		text: {
+			type: String,
+			default: "BUTTON"
+		},
+		//ICON 位置
+		iconPositon: {
+			type: String,
+			default: "LEFT"
+		},
+		//设置圆角按钮 "square" | "circle",
+		shape: {
+			type: String,
+			default: "square"
+		},
+		//是否缕空
+		plain: {
+			type: Boolean,
+			default: false,
 		}
 	})
-	
+
 	/** 
 	 * 按钮状态 0:默认 1:按下  2:禁用
 	 */
 	const buttonState = ref(0)
 
 	onMounted(() => {
-		console.error("onMounted",props.disable);
-		if(props.disable){
-			buttonState.value =2
-		}
+		buttonState.value = props.disabled ?? 0;
 	})
 
 	const onTouchStart = () => {
-		console.error("onTouchStart");
-		if (props.disable || !props.downAnimation) return;
+		if (props.disabled || !props.downAnimation) return;
 		buttonState.value = 1
 	}
 	const onTouchEnd = () => {
-		console.error("onTouchEnd");
-		if (props.disable || !props.downAnimation) return;
+		if (props.disabled || !props.downAnimation) return;
 		buttonState.value = 0
 	}
 	//长按弹窗检查信息
-	const onContextMenu=(e)=>{
+	const onContextMenu = (e) => {
 		e.preventDefault();
 	}
 
-	const buttonBackgroundCls = computed(() => {
-		let state = buttonState.value
-		if (state < 0 || state > 2) {
-			state = 0
+	/**
+	 * 设置文字颜色
+	 **/
+	const getTextColor = computed(() => {
+		const pos = props.iconPositon
+		return {
+			color: props.disabled ? "#666" : buttonState.value == 1 ?
+				props.downTextColor || props.textColor : props.textColor,
+			"margin-left": pos == "LEFT" ? "8rpx" : "0px",
+			"margin-right": pos == "RIGHT" ? "8rpx" : "0px",
+			"margin-top": pos == "TOP" ? "8rpx" : "0px",
+			"margin-bottom": pos == "BOTTOM" ? "8rpx" : "0px",
 		}
-		return [`fui-button-${state}`]
 	})
-	const buttonTextColorCls=computed(()=>{
-		if(!props.disable) return ['button-text']
-		return ['button-text-disable']
-	})
+	/**
+	 * 设置背景颜色
+	 **/
+	const getBackGroundColor = computed(() => {
+		const shape = props.shape;
+		let shapeValue : string = shape == "circle" ? "100rpx" : "6rpx"
+		const defaultColor = props.backGroundColor;
+		const downBgColor = props.downBackGroundColor;
+		let bgColor = props.disabled ? "#ccc" : buttonState.value == 1 ?
+			downBgColor.length > 0 ? downBgColor : defaultColor : defaultColor;
+		const borderColor = `1px solid ${bgColor}`;
+		if (props.plain) {
+			bgColor = "transparent";
+		}
+		return {
+			"background": bgColor,
+			"border-radius": shapeValue,
+			"border": borderColor,
+		}
+	});
 </script>
 
 <style lang="scss" scoped>
 	.fui-button {
 		border-radius: 6rpx;
-		padding: 20rpx 30rpx;
+		padding: 10rpx 20rpx;
+		margin: 0px auto;
+	}
+
+	.button-container {
 		display: flex;
 		flex-direction: row;
 		align-items: center;
 		justify-content: center;
 	}
-	
-	.button-text{
-		color:#FFF;
-		margin-left: 10rpx;
-	}
-	.button-text-disable{
-		color:#ccc;
-		margin-left: 10rpx;
-	}
-	
-	.fui-button-0 {
-		background: gray;
-	}
 
-	.fui-button-1 {
-		background: #ccc;
-		cursor: not-allowed;
-	}
-
-	.fui-button-2 {
-		background: #EEE;
+	.button-text-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 </style>
